b88159bb675ccb798ff56d95bfcfdecf2e3d0f4b
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2008-10-12 17:05:23 +0000

    Added Memcached Communications Layer. Currently using Tummy's Python Client. Needs testing for communications to memcached server(s) in the clusters. Completed delete-keys & delete-multi-keys Blueprints. Needs further testing on the actual deleting of keys.

--- src/Clusters.py
+++ src/Clusters.py
@@ -1,4 +1,5 @@
 import md5
+import memcached.memcache
 
 class Cluster:
     def __init__(self, name):
@@ -15,6 +16,8 @@ class Cluster:
                  }
         self.key = md5.new(self.name).hexdigest()
         
+        self.memcached = memcached.memcache.Client(self.getServerMemcachedUrls(), debug=0)
+        
     def setMenuItems(self, items):
         self.menuItems = items
         
@@ -27,6 +30,13 @@ class Cluster:
         
     def getServers(self):
         return self.servers
+    
+    def getServerMemcachedUrls(self):
+        servers = []
+        for server in self.servers:
+            servers.append(str(server.ip) +":"+ str(server.port))
+            
+        return servers
         
     def save(self):
         save = {'name':self.name, 'servers':[]}
@@ -42,3 +52,7 @@ class Cluster:
             
         if self.menuItems['menu'] is not None:
             self.menuItems['menu'].parent().removeAction(self.menuItems['menu'].menuAction())
+            
+    def deleteKey(self, key):
+        keys = key.split(';')
+        self.memcached.delete_multi(keys)
