5c1bac23cfb1fe20561b629ad059a8cfc5a55032
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2009-01-16 02:17:49 +0000

    Fixed Minor Bug with Tree Delete

--- trunk/src/Clusters.py
+++ trunk/src/Clusters.py
@@ -65,7 +65,7 @@ class Cluster:
 			self.menuItems['menu'].parent().removeAction(self.menuItems['menu'].menuAction())
 			
 		if self.treeItemParent is not None and self.treeItem is not None:
-			self.treeItemParent.removeChild(self.treeItem)
+			self.treeItemParent.removeItemWidget(self.treeItem)
 				
 	def getMemcached(self):
 		return memcached.memcache.Client(self.getServerMemcachedUrls(), debug=0)
