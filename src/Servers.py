3b9df19504feb92aa1d127e438e008ee3a9eab30
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2009-01-16 02:11:51 +0000

    Added Tree Removal on Delete for Clusters and Servers

--- src/Servers.py
+++ src/Servers.py
@@ -2,29 +2,32 @@ from PyQt4.QtGui import QTreeWidgetItem
 from PyQt4.QtCore import QStringList
 
 class Server:
-    def __init__(self, name, ip, port):
-        self.name = name
-        self.ip = ip
-        self.port = port
-        self.menuItems = {'menu': None, 'actions':{'delete': None}}
-        self.cluster = None
-        self.tree = None
-        
-    def initTreeView(self):
-        self.tree = QTreeWidgetItem(self.cluster.treeItem, 
+	def __init__(self, name, ip, port):
+		self.name = name
+		self.ip = ip
+		self.port = port
+		self.menuItems = {'menu': None, 'actions':{'delete': None}}
+		self.cluster = None
+		self.tree = None
+		
+	def initTreeView(self):
+		self.tree = QTreeWidgetItem(self.cluster.treeItem, 
 								QStringList(str(self.name) +" ( "+ str(self.ip) +":"+ str(self.port) +" )"))
-        
-    def save(self):
-        return {'name':self.name, 'ip':self.ip, 'port':self.port}
-    
-    def setMenuItems(self, items):
-        self.menuItems = items
-        
-    def setCluster(self, cluster):
-        self.cluster = cluster
-        
-    def delete(self):
-        if self.menuItems['menu'] is not None:
-            self.menuItems['menu'].parent().removeAction(self.menuItems['menu'].menuAction())
-            
-        self.cluster.deleteServer(self)
+		
+	def save(self):
+		return {'name':self.name, 'ip':self.ip, 'port':self.port}
+	
+	def setMenuItems(self, items):
+		self.menuItems = items
+		
+	def setCluster(self, cluster):
+		self.cluster = cluster
+		
+	def delete(self):
+		if self.menuItems['menu'] is not None:
+			self.menuItems['menu'].parent().removeAction(self.menuItems['menu'].menuAction())
+			
+		if self.cluster.treeItem is not None and self.tree is not None:
+			self.cluster.treeItem.removeChild(self.tree)
+			
+		self.cluster.deleteServer(self)
