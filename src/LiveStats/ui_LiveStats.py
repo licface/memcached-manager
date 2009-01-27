44ed3fa5f66c9cccf2d2c981d8c5f6d1b402174b
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2009-01-27 06:18:28 +0000

    Started Key Search Panel Design Redesigned Management Tasks Tab to add Key Search

--- src/LiveStats/ui_LiveStats.py
+++ src/LiveStats/ui_LiveStats.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'LiveStats.ui'
 #
-# Created: Thu Nov 20 01:18:56 2008
+# Created: Mon Jan 26 23:17:35 2009
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
