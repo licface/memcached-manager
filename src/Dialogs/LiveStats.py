e0b657906574320d4989a1ee9755e971bfdd9522
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2009-03-29 19:21:05 +0000

    Added Ignore Finished the Thread Loop and Toggle in the Live Stats Watcher.

--- src/Dialogs/LiveStats.py
+++ src/Dialogs/LiveStats.py
@@ -3,40 +3,58 @@ from PyQt4 import QtCore
 from Dialogs.ui_LiveStats import Ui_liveStatsDialog
 import time
 import threading
+import Settings
 
 class Dialog(QtGui.QDialog, Ui_liveStatsDialog):
-    def __init__(self):
-        QtGui.QDialog.__init__(self)
-        self.setupUi(self)
-        self.currentCluster = None
-        self.monitor = False
-        
-        self.connect(self, QtCore.SIGNAL('finished(int)'), self.stopMonitor)
-        
-    def show(self):
-        QtGui.QDialog.show(self)
-        self.startMonitor()
-        
-    def setCluster(self, cluster):
-        self.currentCluster = cluster
-        
-    def startMonitor(self):
-        print "Start Monitor"
-        self.monitor = True
-    
-    def stopMonitor(self):
-        print "Stop Monitor"
-        self.monitor = False
-        
-    def toggleMonitor(self):
-        if self.monitor:
-            self.stopMonitor()
-        else:
-            self.startMonitor()
-            
+	def __init__(self):
+		QtGui.QDialog.__init__(self)
+		self.setupUi(self)
+		self.currentCluster = None
+		self.monitor = False
+		self.thread = None
+		self.threadInterupt = False
+		
+		self.settings = Settings.Settings()
+		
+		self.connect(self, QtCore.SIGNAL('finished(int)'), self.stopMonitor)
+		
+	def show(self):
+		QtGui.QDialog.show(self)
+		self.startMonitor()
+		
+	def setCluster(self, cluster):
+		self.currentCluster = cluster
+		
+	def startMonitor(self):
+		print "Start Monitor"
+		self.monitor = True
+		self.thread = Monitor(self)
+		self.thread.start()
+		
+	
+	def stopMonitor(self):
+		print "Stop Monitor"
+		self.monitor = False
+		self.threadInterupt = True
+		
+	def toggleMonitor(self):
+		if self.monitor:
+			self.stopMonitor()
+		else:
+			self.startMonitor()
+			
 class Monitor(threading.Thread):
-    def __init__(self, Dialog):
-        threading.Thread.__init__()
-        
-    def run(self):
-        pass
+	def __init__(self, dialog):
+		threading.Thread.__init__(self)
+		self.dialog = dialog
+		self.stats = []
+		
+	def run(self):
+		while not self.dialog.threadInterupt:
+			stats = self.dialog.currentCluster.getStats()
+			self.stats.append(stats)
+			if len(self.stats) > 20:
+				self.stats.pop(0)
+				
+			print self.stats
+			time.sleep(int(self.dialog.settings.settings.config['Stats']['RefreshInterval']))
