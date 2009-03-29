92cd19f25cf4aa71299616fb6d12504f24d14c9a
nerdynick <nerdynick@4f7a5fa5-b755-0410-8ddb-ff3b00f36568>
2009-03-29 18:29:26 +0000

    Resolved Confilt in MemcachedManager.py Added Total Threads to Cache Info Added Threads to Server Info Added Support for Threads in stats

--- trunk/src/Dialogs/ui_Add.py
+++ trunk/src/Dialogs/ui_Add.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file './Dialogs/Add.ui'
 #
-# Created: Wed Mar 25 19:45:42 2009
+# Created: Sun Mar 29 12:27:48 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
